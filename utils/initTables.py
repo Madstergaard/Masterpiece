deaaed6011f3e26465c33752d7d7cbbcbd7e00ea
jyang8 <jyang8@stuy.edu>
2017-01-15 23:05:22 -0500

    changed initTables to match the updated version

--- utils/initTables.py
+++ utils/initTables.py
@@ -5,7 +5,7 @@ db = sqlite3.connect("data/database.db")
 c = db.cursor()
 cmd = "CREATE TABLE IF NOT EXISTS accounts(user TEXT, hashedPass TEXT, userID INTEGER);"
 c.execute(cmd)
-cmd = "CREATE TABLE IF NOT EXISTS docs(title TEXT, content TEXT, userID INTEGER, status TEXT, comments TEXT);"
+cmd = "CREATE TABLE IF NOT EXISTS docs(title TEXT, content TEXT, userID INTEGER, status TEXT, comments TEXT, description TEXT, coverURL TEXT, authors TEXT);"
 c.execute(cmd)
 
 db.commit()
