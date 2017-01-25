785c8b6038d3adbdb21eb1d74afc40c356fd2d65
Maddie Ostergaard <Mostergaard@stuy.edu>
2017-01-24 20:16:28 -0500

    integrated Google Sign In

---
+++ utils/docs.py
@@ -0,0 +1,23 @@
+import json 
+import requests 
+import base64 
+import urllib 
+import uuid 
+
+# Log in ---------------------------vv--------------------------
+
+# Client Keys
+CLIENT_ID = json.loads(open("keys.json").read())['web']['client_id'] 
+CLIENT_SECRET = json.loads(open("keys.json").read())['web']['client_secret'] 
+
+# Google URLS
+GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/auth"
+GOOGLE_TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
+
+# Server-side Parameters
+REDIRECT_URI = 'http://localhost:5000/home/'
+
+
+# Create doc ---------------------------vv--------------------------
+
+#
